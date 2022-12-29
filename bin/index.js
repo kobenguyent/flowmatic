dfa520f28b74756db9e15561d702a9b6a9456d61
PeterNgTr <peter.nguyentr@gmail.com>
2022-12-29 11:31:12 +0100

    fix: adding bin folder

--- bin/index.js
+++ bin/index.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import chalk from 'chalk';
 import inquirer from 'inquirer';
-import { fileNameFormat, getCurrentWorkingDir} from "./utils/fileHelper.js";
+import { fileNameFormat, getCurrentWorkingDir} from "../utils/fileHelper.js";
 
-import {data} from "./data/data.js";
-import {createPipeline} from "./utils/pipelinesHelper.js";
+import {data} from "../data/data.js";
+import {createPipeline} from "../utils/pipelinesHelper.js";
 
 import CFonts from 'cfonts';
 
