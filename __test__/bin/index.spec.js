844316fa1206df9844e903a45cd1912e1a6d82ef
peterngtr <peter.nguyentr@gmail.com>
2023-01-05 16:31:01 +0100

    fix: UTs

--- __test__/bin/index.spec.js
+++ __test__/bin/index.spec.js
@@ -7,7 +7,7 @@ const runner = path.join(process.cwd(), './bin/index.js');
 describe('Flowmatic CLI', () => {
     describe('flows', () => {
         test('should see full flow', async () => {
-            let result = await run([runner], [ENTER, ENTER]);
+            let result = await run([runner], [ENTER, ENTER, ENTER]);
             expect(result).toContain('Pipeline Creation at ease - supporting');
             expect(result).toContain('Which CI/CD are you currently using?');
 
