539783d07169a52db3920e9bcaa96061f27fd064
kobenguyent <kobenguyent@gmail.com>
2023-03-22 13:10:48 +0100

    fix: test

--- __test__/bin/index.spec.js
+++ __test__/bin/index.spec.js
@@ -1,6 +1,6 @@
 import run from "inquirer-test";
 import path from "path";
-import { DOWN, ENTER } from "inquirer-test";
+import { ENTER } from "inquirer-test";
 
 const runner = path.join(process.cwd(), "./bin/index.js");
 
@@ -10,11 +10,7 @@ describe("Flowmatic CLI", () => {
 			let result = await run([runner], [ENTER, ENTER, ENTER]);
 			expect(result).toContain("Pipeline Creation at ease - supporting");
 			expect(result).toContain("Which CI/CD are you currently using?");
-
-			expect(result).toContain(
-				"Do you want to publish nodejs package from GitHub to npm?",
-			);
-
+			expect(result).toContain("Do you want to publish nodejs package from GitHub to npm?");
 			expect(result).toContain("Which Node version do you want to use?");
 			expect(result).toContain("16");
 			expect(result).toContain("18");
